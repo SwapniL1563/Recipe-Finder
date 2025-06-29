886d38f437d1a4547bdc4507414a1e7ed6ce2416
SwapniL1563 <choratswapnil15@gmail.com>
2025-06-30 00:44:39 +0530

    Added rate limit to fetch route to prevent API overuse

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from "cors"
 import connectDB from './config/connectDB.js';
 import recipeRoutes from "./routes/recipeRoutes.js"
+import rateLimit from "express-rate-limiter";
 
 const PORT = 5000;
 
@@ -9,6 +10,17 @@ const app = express();
 
 // connect to DB
 connectDB();
+
+// rate limiter per user or Ip
+const fetchRateLimiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000,
+    max:10,
+    message:{
+    status: 429,
+    message: "You have reached the daily recipe search limit. Please try again tomorrow.",
+
+    }
+})
 
 // cors options - allow frontend
 const corsOptions = {
@@ -19,6 +31,9 @@ const corsOptions = {
 // middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+// rate limit
+app.use("/api/recipe/fetch",fetchRateLimiter)
 
 // routes
 app.use("/api/recipe",recipeRoutes);
